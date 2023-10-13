98ef963c85502308e6f54db713d4bc0cf7b7a060
MasukBD <imahfuz2020@outlook.com>
2023-10-14 00:17:58 +0600

    banner section updated

--- src/Pages/Home/Carousel/Banner.jsx
+++ src/Pages/Home/Carousel/Banner.jsx
@@ -14,7 +14,7 @@ const Banner = () => {
                 <img src={img1} className="w-full rounded-md" />
                 <div className="absolute lg:right-0 flex rounded-md items-center justify-end h-full lg:bg-gradient-to-l from-[#151515] to-[rgba(21, 21, 21, 0.00)] bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                     <div className='w-full lg:w-1/2 space-y-3 lg:space-y-7 pl-5 text-white'>
-                        <h1 className='lg:text-6xl text-3xl font-bold'>Maintenance And Services With Best Price In Dhaka</h1>
+                        <h1 className='lg:text-6xl text-2xl font-bold'>Maintenance And Services With Best Price In Dhaka</h1>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form!</p>
                         <div className='flex gap-7'>
                             <Link to="/service"><button className='btn btn-error'>Services</button></Link>
@@ -22,16 +22,16 @@ const Banner = () => {
                         </div>
                     </div>
                 </div>
-                <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 right-5 lg:right-10 bottom-0 lg:bottom-5">
-                    <a href="#slide4" className='text-4xl hover:text-red-500'><FaArrowCircleLeft></FaArrowCircleLeft></a>
-                    <a href="#slide2" className='text-4xl hover:text-red-500'><FaArrowCircleRight></FaArrowCircleRight></a>
+                <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 right-2 lg:right-10 bottom-0 lg:bottom-5">
+                    <a href="#slide4" className='text-2xl lg:text-4xl text-white hover:text-red-500'><FaArrowCircleLeft></FaArrowCircleLeft></a>
+                    <a href="#slide2" className='text-2xl lg:text-4xl text-white hover:text-red-500'><FaArrowCircleRight></FaArrowCircleRight></a>
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
                 <img src={img2} className="w-full rounded-md" />
                 <div className="absolute lg:right-0 rounded-md flex items-center justify-end h-full lg:bg-gradient-to-l from-[#151515] to-[rgba(21, 21, 21, 0.00)] bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                     <div className='w-full lg:w-1/2 space-y-3 lg:space-y-7 pl-5 text-white'>
-                        <h1 className='lg:text-6xl text-3xl font-bold'>Maintenance And Services With Best Price In Dhaka</h1>
+                        <h1 className='lg:text-6xl text-2xl font-bold'>Maintenance And Services With Best Price In Dhaka</h1>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form!</p>
                         <div className='flex gap-7'>
                             <Link to="/service"><button className='btn btn-error'>Services</button></Link>
@@ -39,16 +39,16 @@ const Banner = () => {
                         </div>
                     </div>
                 </div>
-                <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 right-5 lg:right-10 bottom-0 lg:bottom-5">
-                    <a href="#slide1" className='text-4xl hover:text-red-500'><FaArrowCircleLeft></FaArrowCircleLeft></a>
-                    <a href="#slide3" className='text-4xl hover:text-red-500'><FaArrowCircleRight></FaArrowCircleRight></a>
+                <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 right-2 lg:right-10 bottom-0 lg:bottom-5">
+                    <a href="#slide1" className='text-2xl lg:text-4xl text-white hover:text-red-500'><FaArrowCircleLeft></FaArrowCircleLeft></a>
+                    <a href="#slide3" className='text-2xl lg:text-4xl text-white hover:text-red-500'><FaArrowCircleRight></FaArrowCircleRight></a>
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
                 <img src={img3} className="w-full rounded-md" />
                 <div className="absolute lg:left-0 rounded-md flex items-center justify-start h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                     <div className='w-full lg:w-1/2 space-y-3 lg:space-y-7 pl-5 text-white'>
-                        <h1 className='lg:text-6xl text-3xl font-bold'>Maintenance And Services With Best Price In Dhaka</h1>
+                        <h1 className='lg:text-6xl text-2xl font-bold'>Maintenance And Services With Best Price In Dhaka</h1>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form!</p>
                         <div className='flex gap-7'>
                             <Link to="/service"><button className='btn btn-error'>Services</button></Link>
@@ -56,16 +56,16 @@ const Banner = () => {
                         </div>
                     </div>
                 </div>
-                <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 right-5 lg:right-10 bottom-0 lg:bottom-5">
-                    <a href="#slide2" className='text-4xl hover:text-red-500'><FaArrowCircleLeft></FaArrowCircleLeft></a>
-                    <a href="#slide4" className='text-4xl hover:text-red-500'><FaArrowCircleRight></FaArrowCircleRight></a>
+                <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 right-2 lg:right-10 bottom-0 lg:bottom-5">
+                    <a href="#slide2" className='text-2xl lg:text-4xl text-white hover:text-red-500'><FaArrowCircleLeft></FaArrowCircleLeft></a>
+                    <a href="#slide4" className='text-2xl lg:text-4xl text-white hover:text-red-500'><FaArrowCircleRight></FaArrowCircleRight></a>
                 </div>
             </div>
             <div id="slide4" className="carousel-item relative w-full">
                 <img src={img4} className="w-full rounded-md" />
                 <div className="absolute lg:left-0 rounded-md flex items-center justify-start h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                     <div className='w-full lg:w-1/2 space-y-3 lg:space-y-7 pl-5 text-white'>
-                        <h1 className='lg:text-6xl text-3xl font-bold'>Maintenance And Services With Best Price In Dhaka</h1>
+                        <h1 className='lg:text-6xl text-2xl font-bold'>Maintenance And Services With Best Price In Dhaka</h1>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form!</p>
                         <div className='flex gap-7'>
                             <Link to="/service"><button className='btn btn-error'>Services</button></Link>
@@ -73,9 +73,9 @@ const Banner = () => {
                         </div>
                     </div>
                 </div>
-                <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 right-5 lg:right-10 bottom-0 lg:bottom-5">
-                    <a href="#slide3" className='text-4xl hover:text-red-500'><FaArrowCircleLeft></FaArrowCircleLeft></a>
-                    <a href="#slide1" className='text-4xl hover:text-red-500'><FaArrowCircleRight></FaArrowCircleRight></a>
+                <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 right-2 lg:right-10 bottom-0 lg:bottom-5">
+                    <a href="#slide3" className='text-2xl lg:text-4xl text-white hover:text-red-500'><FaArrowCircleLeft></FaArrowCircleLeft></a>
+                    <a href="#slide1" className='text-2xl lg:text-4xl text-white hover:text-red-500'><FaArrowCircleRight></FaArrowCircleRight></a>
                 </div>
             </div>
         </div>
